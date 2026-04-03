58e66ad0f0629fdbf9361314b179a4ba06378ce4
Wen Tzu (Joko) Chang 張紋慈 <99936530+jojoAxolotl@users.noreply.github.com>
2026-04-03 13:12:17 +0800

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+
+        return dp[0]
+# bottom-up dynamic programming approach
+# Time complexity: O(n*m*t) n: len(s) m: len(wordDict) 
+# t: he maximum length of any word in wordDict
+# Space complexity: O(n)
